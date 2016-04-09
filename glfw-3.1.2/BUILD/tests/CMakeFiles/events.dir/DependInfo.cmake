e79b61ff9642de5aca79db51646ecc758ef91114
semtexzv <semtexzv@gmail.com>
2016-04-09 14:18:30 +0200

    More effort to get both windows and Linux working on GLFW

---
+++ glfw-3.1.2/BUILD/tests/CMakeFiles/events.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/semtexzv/XFG/glfw-3.1.2/deps/getopt.c" "/home/semtexzv/XFG/glfw-3.1.2/BUILD/tests/CMakeFiles/events.dir/__/deps/getopt.c.o"
+  "/home/semtexzv/XFG/glfw-3.1.2/tests/events.c" "/home/semtexzv/XFG/glfw-3.1.2/BUILD/tests/CMakeFiles/events.dir/events.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../deps"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/semtexzv/XFG/glfw-3.1.2/BUILD/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
